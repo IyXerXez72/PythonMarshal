ad6192b6d13f428d2ea9d07a1674a98ee8a911dd
InYourXerXez <51115343+IyXerXez72@users.noreply.github.com>
2019-06-02 08:02:48 +0700

    Add files via upload

---
+++ au.sh
@@ -0,0 +1,21 @@
+##
+##""""
+###OKE SAYA MAU MEMINTA MAAF PLIS JAN RECODE TOOLS KU WALAU AKU MASIH NOOB TAPI SAYA BERUSAHA UNTUK MEMBUATYA PLIS LAH JAN DI RECODE WALAW BRANTAKAN JUGA HEHE ;) RECODE WAR KITA AJG
+##""""
+
+clear
+sleep 2
+read -p "masukan nama mu =>" nama;
+clear
+echo "				HAI $nama SLAMAT DATANG"|lolcat
+echo "		{|}---------------------------------------{|}"|lolcat
+echo "		{!} AUTHOR = XerXez7                      {!}"|lolcat
+echo "		{?} TEAM   = 2E4H-GBLGCREW-BOGOR BLACK HAT{?}"|lolcat
+echo "		{i} SINCE  = 2K19-2K20                    {i}"|lolcat
+echo "		{f} CompiRinght = @InYourXerXez           {f}"|lolcat
+echo "		{c} Comtack= noxerxez233@gmail.com        {c}"|lolcat
+echo "		{|}---------------------------------------{|}"|lolcat
+echo "				InYourXerXez7]"|lolcat
+echo "				SILAHKAN PILIH X/Q"|lolcat
+echo "			(Q)=Exit		(X)=Encript"|lolcat
+
